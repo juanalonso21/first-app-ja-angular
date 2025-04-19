cf5b0d08c325b741effe5e4aef976a11db74d975
juanalonso21 <juanalonsopulpi@gmail.com>
2025-04-19 15:24:08 +0200

    JAP: Creacion del componente home y llamandolo en el app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import {Component} from '@angular/core';
-
+import {HomeComponent} from './home/home.component';
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Hello world!</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
+  template: `    
+  <main>
+  <header class="brand-name">
+    HOMES
+  </header>
+  <section class="content">
+    <app-home></app-home>
+  </section>
+</main>
+`,
+styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
