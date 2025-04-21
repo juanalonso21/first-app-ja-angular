25caf9b53d6bb35b9a57c45f0a510512b4e2da97
juanalonso21 <juanalonsopulpi@gmail.com>
2025-04-21 21:53:58 +0200

    JAP: Cambios de creacion del router y creacion de la pagina de detalle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from "@angular/core";
-import { HomeComponent } from "./home/home.component";
-
+import {Component} from '@angular/core';
+ import { HomeComponent } from './home/home.component';
+ import { RouterOutlet, RouterLink } from '@angular/router';
+ import { HousingLocationComponent } from './housing-location/housing-location.component';
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [RouterOutlet, RouterLink],
   template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="logo.svg" alt="logo" aria-hidden="true" />
-      </header>
-      <section class="content">
-        <app-home></app-home>
+<main>
+       <a [routerLink]="['/']">
+         <header class="brand-name">
+           <img class="brand-logo" src="logo.svg" alt="logo" aria-hidden="true" />
+         </header>
+       </a>
+       <section class="content">
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
