fef026da5a6d10364b60a76d6dd68418b2684a60
juanalonso21 <juanalonsopulpi@gmail.com>
2025-04-22 00:12:56 +0200

    JAP: Buscador con funcionalidad

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,21 +1,28 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocation} from '../housinglocation';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-housing-location',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
-<section class="listing">
-      <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
-      />
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
-    </section>
-  `,
+  <section class="listing">
+    <img
+      class="listing-photo"
+      [src]="housingLocation.photo"
+      alt="Exterior photo of {{ housingLocation.name }}"
+      crossorigin
+    />
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+    <a
+      class="learn-more-button"
+      [routerLink]="['/details', housingLocation.id]"
+    >
+      Learn more
+    </a>
+  </section>
+`,
   styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
