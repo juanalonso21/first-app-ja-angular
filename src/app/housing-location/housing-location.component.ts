9198286552166ee11dffbf89b87d9a0a81d9d1c8
juanalonso21 <juanalonsopulpi@gmail.com>
2025-04-21 19:58:18 +0200

    JAP: Añado un input a el housing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
-  imports: [],
-  templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.css'
+  imports: [CommonModule],
+  template: `
+    <p>housing-location works!</p>
+  `,
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
